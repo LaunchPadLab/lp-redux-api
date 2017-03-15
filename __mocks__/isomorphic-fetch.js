696193bf173dfc9b27df2497172d6c4d54ca4e46
David Pickart <davidstoreypickart@gmail.com>
2017-03-15 10:32:55 -0500

    Add network error test

--- __mocks__/isomorphic-fetch.js
+++ __mocks__/isomorphic-fetch.js
@@ -1,13 +1,16 @@
 export const successUrl = '/success'
 export const failureUrl = '/failure'
+export const networkErrorUrl = '/network-error'
 
 export default jest.fn(function (url) {
   const response = { 
     json: () => Promise.resolve({}),
-    ok: url !== failureUrl 
+    ok: (url !== failureUrl) 
   }
   // Simulate server response
-  return new Promise((resolve) => {
-    setTimeout(resolve(response), 10)
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      () => (url === networkErrorUrl) ? reject('Network error') : resolve(response)
+    , 10)
   })
 })
--- test/middleware.test.js
+++ test/middleware.test.js
@@ -1,6 +1,6 @@
 jest.mock('isomorphic-fetch')
 
-import { successUrl, failureUrl } from 'isomorphic-fetch'
+import { successUrl, failureUrl, networkErrorUrl } from 'isomorphic-fetch'
 import { middleware, LP_API } from '../src'
 import { lpApiRequest, lpApiSuccess, lpApiFailure } from '../src/actions'
 
@@ -29,19 +29,13 @@ const ACTION_TYPE_REQUEST = 'ACTION_TYPE_REQUEST'
 const ACTION_TYPE_SUCCESS = 'ACTION_TYPE_SUCCESS'
 const ACTION_TYPE_FAILURE = 'ACTION_TYPE_FAILURE'
 
-const successfulAction = {
-  [LP_API]: {
-    url: successUrl,
-    requestKey: REQUEST_KEY,
-    actions: [ACTION_TYPE_REQUEST, ACTION_TYPE_SUCCESS, ACTION_TYPE_FAILURE]
-  }
-}
-
-const failingAction = {
-  [LP_API]: {
-    url: failureUrl,
-    requestKey: REQUEST_KEY,
-    actions: [ACTION_TYPE_REQUEST, ACTION_TYPE_SUCCESS, ACTION_TYPE_FAILURE]
+const actionWithURL = (url) => {
+  return {
+    [LP_API]: {
+      url,
+      requestKey: REQUEST_KEY,
+      actions: [ACTION_TYPE_REQUEST, ACTION_TYPE_SUCCESS, ACTION_TYPE_FAILURE]
+    }
   }
 }
 
@@ -57,43 +51,50 @@ test('middleware requires user and actions/types arguments', () => {
 })
 
 test('middleware dispatches reducer REQUEST action', () => {
-  return callMiddleware(successfulAction, { waitForActions: 1 }).then((actions) => {
+  return callMiddleware(actionWithURL(successUrl), { waitForActions: 1 }).then((actions) => {
     const apiRequestAction = actions.pop()
     expect(apiRequestAction).toEqual(lpApiRequest(REQUEST_KEY))
   })
 })
 
 test('middleware dispatches user-defined REQUEST action', () => {
-  return callMiddleware(successfulAction, { waitForActions: 2 }).then((actions) => {
+  return callMiddleware(actionWithURL(successUrl), { waitForActions: 2 }).then((actions) => {
     const userRequestAction = actions.pop()
     expect(userRequestAction.type).toEqual(ACTION_TYPE_REQUEST)
   })
 })
 
 test('middleware dispatches reducer SUCCESS action', () => {
-  return callMiddleware(successfulAction, { waitForActions: 3 }).then((actions) => {
+  return callMiddleware(actionWithURL(successUrl), { waitForActions: 3 }).then((actions) => {
     const apiSuccessAction = actions.pop()
     expect(apiSuccessAction).toEqual(lpApiSuccess(REQUEST_KEY))
   })
 })
 
 test('middleware dispatches user-defined SUCCESS action', () => {
-  return callMiddleware(successfulAction, { waitForActions: 4 }).then((actions) => {
+  return callMiddleware(actionWithURL(successUrl), { waitForActions: 4 }).then((actions) => {
     const userSuccessAction = actions.pop()
     expect(userSuccessAction.type).toEqual(ACTION_TYPE_SUCCESS)
   })
 })
 
 test('middleware dispatches reducer FAILURE action', () => {
-  return callMiddleware(failingAction, { waitForActions: 3 }).then((actions) => {
+  return callMiddleware(actionWithURL(failureUrl), { waitForActions: 3 }).then((actions) => {
     const apiSuccessAction = actions.pop()
     expect(apiSuccessAction).toEqual(lpApiFailure(REQUEST_KEY))
   })
 })
 
 test('middleware dispatches user-defined FAILURE action', () => {
-  return callMiddleware(failingAction, { waitForActions: 4 }).then((actions) => {
+  return callMiddleware(actionWithURL(failureUrl), { waitForActions: 4 }).then((actions) => {
     const userSuccessAction = actions.pop()
     expect(userSuccessAction.type).toEqual(ACTION_TYPE_FAILURE)
+  })
+})
+
+test('middleware dispatches reducer FAILURE action on network error', () => {
+  return callMiddleware(actionWithURL(networkErrorUrl), { waitForActions: 3 }).then((actions) => {
+    const apiSuccessAction = actions.pop()
+    expect(apiSuccessAction).toEqual(lpApiFailure(REQUEST_KEY))
   })
 })
