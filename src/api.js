7312da48409e8043e1fffcf6aaa7cdff8632dbba
David Corwin <dave@launchpadlab.com>
2017-04-04 15:37:21 -0500

    add destroy method for deletes, allow api methods to take options

--- src/api.js
+++ src/api.js
@@ -5,39 +5,53 @@ const OPTIONS = {
   body: null,
 }
 
-export function get (url) {
+export function get(url, opts = {}) {
   return http(url, {
     ...OPTIONS,
+    ...opts,
   })
 }
 
-export function patch (url, body) {
+export function patch(url, body, opts = {}) {
   return http(url, {
     ...OPTIONS,
+    ...opts,
     body,
     method: 'PATCH',
   })
 }
 
-export function post (url, body) {
+export function post(url, body, opts = {}) {
   return http(url, {
     ...OPTIONS,
+    ...opts,
     body,
     method: 'POST',
   })
 }
 
-export function put (url, body) {
+export function put(url, body, opts = {}) {
   return http(url, {
     ...OPTIONS,
+    ...opts,
     body,
     method: 'PUT',
   })
 }
 
-export function call (url, method, body) {
+export function destroy(url, body, opts = {}) {
   return http(url, {
     ...OPTIONS,
+    ...opts,
+    body,
+    method: 'DELETE',
+  })
+}
+
+export function call(url, method, body, opts = {}) {
+  return http(url, {
+    ...OPTIONS,
+    ...opts,
     body,
     method,
   })
