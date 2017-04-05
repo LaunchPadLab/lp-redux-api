ba837273d201a123af39ea2ae9a6c2c2be1ee4f1
Rachel Killackey <rachel@launchpadlab.com>
2017-04-05 16:29:42 -0500

    grab cookie context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ export * as api from './api'
 export { default as http } from './http'
 export { default as HttpError } from './http-error'
 export { default as isAuthenticated } from './is-authenticated'
+export { default as isAuthenticatedWithContext } from './is-authenticated-with-context'
+export { default as getContext } from './get-context'
 export { default as LP_API } from './LP_API'
 export { default as middleware } from './middleware'
 export { default as reducer, selectStatus } from './reducer'
-export { 
-  LP_API_STATUS_LOADING, 
-  LP_API_STATUS_SUCCESS, 
-  LP_API_STATUS_FAILURE 
+export {
+  LP_API_STATUS_LOADING,
+  LP_API_STATUS_SUCCESS,
+  LP_API_STATUS_FAILURE
 } from './actions'
