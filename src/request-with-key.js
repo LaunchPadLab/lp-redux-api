749459087f47a2893dad02449b9a23bc923a5c48
David Pickart <davidstoreypickart@gmail.com>
2017-04-05 16:04:26 -0500

    Add forbidden options

--- src/request-with-key.js
+++ src/request-with-key.js
@@ -1,7 +1,12 @@
 import LP_API from './LP_API'
+import { hasOverlap } from './utils'
 
-export default function (requestKey, options={}) {
-  if (!requestKey) throw `Must include 'requestKey' argument`
+
+
+export default function requestWithKey (requestKey, options={}) {
+  if (!requestKey) throw 'Must include a key for requestWithKey() request.'
+  const providedOpts = Object.keys(options)
+  if (hasOverlap(providedOpts, actionOpts)) throw 'Cannot speciify actions when using requestWithKey()'
   return {
     [LP_API]: {
       ...options,
@@ -10,3 +15,6 @@ export default function (requestKey, options={}) {
     }
   }
 }
+
+// Action-related options that are forbidden
+const actionOpts = ['actions', 'types', 'requestAction', 'successAction', 'failureAction']
