c669727538618e85d3466eeb11eace4af9c55072
Rachel Killackey <rachel@launchpadlab.com>
2017-04-07 11:08:41 -0500

    update variable name

--- src/is-authenticated-with-context.js
+++ src/is-authenticated-with-context.js
@@ -1,7 +1,7 @@
 import { getLpAuthCookie, parseObject } from './utils'
 
-export default function (ctxPath) {
+export default function (ctxStr) {
   const lpAuthCookie = getLpAuthCookie()
   const cookieObj = parseObject(lpAuthCookie)
-  return cookieObj ? cookieObj.context === ctxPath : false
+  return cookieObj ? cookieObj.context === ctxStr : false
 }
