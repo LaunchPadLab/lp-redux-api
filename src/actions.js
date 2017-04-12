edd7cdbe46e4a5fa865ac15bfa5cbf97b97d5c0f
David Pickart <davidstoreypickart@gmail.com>
2017-04-12 11:05:25 -0500

    Add comments to actions

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,12 @@
+// API status constants:
+
 export const LP_API_ACTION = 'LP_API_ACTION'
 export const LP_API_STATUS_LOADING = 'loading'
 export const LP_API_STATUS_SUCCESS = 'success'
 export const LP_API_STATUS_FAILURE = 'failure'
+
+// API status action creator:
+// (these actions are handled by reducer.js)
 
 function lpApiAction (key, status) {
   return {
@@ -9,6 +14,9 @@ function lpApiAction (key, status) {
     payload: { key, status }
   }
 }
+
+// Specific status action creators:
+// (used in middleware.js)
 
 export function lpApiRequest (key) {
   return lpApiAction(key, LP_API_STATUS_LOADING)
