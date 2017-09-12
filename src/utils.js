6b5749dd66702d833d26cdd0c6266fb3a0dc79b5
David Pickart <davidstoreypickart@gmail.com>
2017-09-12 17:05:42 -0500

    Remove unused utils

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,6 @@
 import omitBy from 'lodash/omitBy'
 import isUndefined from 'lodash/isUndefined'
 import overlap from 'lodash/intersection'
-import attempt from 'lodash/attempt'
-import isError from 'lodash/isError'
-import Cookies from 'js-cookie'
 
 export { default as get } from 'lodash/fp/get'
 export { default as set } from 'lodash/fp/set'
@@ -26,17 +23,4 @@ export function hasOverlap (arr1, arr2) {
 // with all keys with undefined values removed
 export function omitUndefined (obj) {
   return omitBy(obj, isUndefined)
-}
-
-// Parses an object from a string and returns the result
-// Returns undefined if there's a parsing error
-export function parseObject (str) {
-  const parsedObj = attempt(() => JSON.parse(str))
-  if (!isError(parsedObj)) return parsedObj
-}
-
-// Get the auth cookie used by lp_token_auth
-// Returns undefined if the cookie is not set
-export function getLpAuthCookie () {
-  return Cookies.get('lp_auth')
 }
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,6 @@
 import { 
   hasOverlap,
   omitUndefined,
-  parseObject,
 } from '../src/utils'
 
 test('hasOverlap correctly determines whether two arrays contain common elements', () => {
@@ -13,15 +12,4 @@ test('omitUndefined omits keys with undefined values', () => {
   const withUndef = { test: true, omit: undefined }
   const withoutUndef = { test: true }
   expect(omitUndefined(withUndef)).toEqual(withoutUndef)
-})
-
-test('parseObject returns a parsed object from a string', () => {
-  const obj = { test: true }
-  const objString = JSON.stringify(obj)
-  expect(parseObject(objString)).toEqual(obj)
-})
-
-test('parseObject returns undefined if string cannot be parsed', () => {
-  const objString = 'not valid'
-  expect(parseObject(objString)).toEqual(undefined)
 })
