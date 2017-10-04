65d940724715d8dba5c9c60e6d2119a261d63593
David Pickart <davidstoreypickart@gmail.com>
2017-10-04 14:52:09 -0500

    Throw error objects instead of strings

--- src/middleware/parse-action.js
+++ src/middleware/parse-action.js
@@ -1,13 +1,13 @@
 // Create an action from an action "definition."
 function parseAction ({ action, payload={}, error=false }) {
   switch (typeof action) {
-  // If it's an action creator, create the action
-  case 'function': return action(payload)
-  // If it's an action object return the action
-  case 'object': return action
-  // Otherwise, create a "default" action object with the given type
-  case 'string': return { type: action, payload, error }
-  default: throw 'Invalid action definition (must be function, object or string).'
+    // If it's an action creator, create the action
+    case 'function': return action(payload)
+    // If it's an action object return the action
+    case 'object': return action
+    // Otherwise, create a "default" action object with the given type
+    case 'string': return { type: action, payload, error }
+    default: throw new Error('Invalid action definition (must be function, object or string).')
   }
 }
 
