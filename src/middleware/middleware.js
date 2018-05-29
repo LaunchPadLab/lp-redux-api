bd219883837d07e536cdda87e4310f665084da56
David Pickart <davidstoreypickart@gmail.com>
2018-05-29 16:13:59 -0500

    Allow falsey responses (#103)
    * Reorder catch statement
    
    * Use one .then
    
    * Version bump: 5.1.2

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -107,33 +107,35 @@ function middleware (options={}) {
     if (requestKey) next(actions.setStatusLoading(requestKey))
     // Make the request
     return http(url, requestOptions)
-      .catch(error => {
-        // Send user-specified failure action
-        if (failureAction) {
-          next(parseAction({
-            action: failureAction,
-            payload: error,
-            error: true,
-          }))
+      .then(
+        // Success handler
+        response => {
+          // Send user-specified success action
+          if (successAction) {
+            next(parseAction({
+              action: successAction,
+              payload: response,
+            }))
+          }
+          // Send success action to API reducer
+          if (requestKey) next(actions.setStatusSuccess(requestKey, response))
+        },
+        // Error handler
+        error => {
+          // Send user-specified failure action
+          if (failureAction) {
+            next(parseAction({
+              action: failureAction,
+              payload: error,
+              error: true,
+            }))
+          }
+          // Send failure action to API reducer
+          if (requestKey) next(actions.setStatusFailure(requestKey))
+          // Dispatch unauthorized action if applicable
+          if (error.status === 401 && onUnauthorized) next(onUnauthorized())
         }
-        // Send failure action to API reducer
-        if (requestKey) next(actions.setStatusFailure(requestKey))
-        // Dispatch unauthorized action if applicable
-        if (error.status === 401 && onUnauthorized) next(onUnauthorized())
-      })
-      .then(response => {
-        // An error was handled above
-        if (!response) return
-        // Send user-specified success action
-        if (successAction) {
-          next(parseAction({
-            action: successAction,
-            payload: response,
-          }))
-        }
-        // Send success action to API reducer
-        if (requestKey) next(actions.setStatusSuccess(requestKey, response))
-      })
+      )
   }
 }
 
