22d8c9638f2679f764b287c499648c07d6dfa3ae
David Pickart <davidstoreypickart@gmail.com>
2019-05-03 16:02:03 -0500

    Status

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -3,10 +3,16 @@ export const failureUrl = '/failure'
 export const unauthorizedUrl = '/unauthorized'
 export const networkErrorUrl = '/network-error'
 
+function getStatus (url) {
+  if (url === failureUrl) return 422
+  if (url === unauthorizedUrl) return 401
+  return 200
+}
+
 export default jest.fn((options) => {
   const url = options.url
   const isError = [failureUrl, unauthorizedUrl].includes(url)
-  const status = isError ? 401 : 200
+  const status = getStatus(url)
   // Send back options as data
   const response = { status, data: options }
   return isError
